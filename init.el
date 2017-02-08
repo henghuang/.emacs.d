32ce8e354b109a03f17e0fde47f2834ee33419d4
henghuang <i@henghuang.me>
2017-02-08 18:27:53 -0500

    could be use now

--- init.el
+++ init.el
@@ -10,7 +10,11 @@
                       flycheck ;; add the flycheck package
                       py-autopep8
                       markdown-mode
-                      paredit))
+                      paredit
+                      neotree
+                      dracula-theme
+                      sublime-themes
+                      auto-complete))
 
 (package-initialize)
 (dolist (p my-packages)
@@ -23,6 +27,7 @@
 
 (setq inhibit-startup-message t) ;; hide the startup messa
 (global-linum-mode t) ;; enable line numbers globall
+(ac-config-default)
 
 ;; PYTHON  
 (elpy-enable)
@@ -31,14 +36,38 @@
   (add-hook 'elpy-mode-hook 'flycheck-mode))
 (require 'py-autopep8)
 (add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)
-;;(elpy-use-ipython)
+
+;; Matlab via buit-in octave-mode
+(setq auto-mode-alist
+      (cons
+       '("\\.m$" . octave-mode)
+       auto-mode-alist))
+
+;;neotree
+(require 'neotree)
+(global-set-key [f8] 'neotree-toggle)
+(setq neo-theme 'arrow)
+
+;;paredit 
+(autoload 'enable-paredit-mode "paredit" "Turn on pseudo-structural editing of Lisp code." t)
+(add-hook 'emacs-lisp-mode-hook       #'enable-paredit-mode)
+(add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
+(add-hook 'ielm-mode-hook             #'enable-paredit-mode)
+(add-hook 'lisp-mode-hook             #'enable-paredit-mode)
+(add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
+(add-hook 'scheme-mode-hook           #'enable-paredit-mode)
+(add-hook 'python-mode-hook #'enable-paredit-mode)
 
 ;;;;;
 ;;customize theme
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/moe-theme.el/")
-(add-to-list 'load-path "~/.emacs.d/themes/moe-theme.el/")
-(require 'moe-theme)
-(load-theme 'moe-dark t)
+
+;(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
+;(load-theme 'dracula t)
+(load-theme 'granger t)
+
+;; Preeeetty font in Emacs 24/Ubuntu
+(set-default-font "Source Code Pro Medium")
+
 ;; init.el ends here
 
 
